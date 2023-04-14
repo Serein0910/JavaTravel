ba63326fab16e8f8cc564238794396ded6b7dfd2
zh <zserein@icloud.com>
2023-04-14 17:36:49 +0800

    旅游网站

---
+++ src/main/resources/admin/admin/src/utils/base.js
@@ -0,0 +1,16 @@
+const base = {
+    get() {
+                return {
+            url : "http://localhost:8080/ssm0wu8j/",
+            name: "ssm0wu8j",
+            // 退出到首页链接
+            indexUrl: 'http://localhost:8080/ssm0wu8j/front/index.html'
+        };
+            },
+    getProjectName(){
+        return {
+            projectName: "旅游管理系统"
+        } 
+    }
+}
+export default base
